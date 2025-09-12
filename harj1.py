0d055037ad50d021ffc57738b784c5462c7ca2f2
Mika Pirttimäki <mika.pirttimaki@centria.fi>
2025-09-12 09:48:11 +0300

    init commit

---
+++ harj1.py
@@ -0,0 +1,8 @@
+import harj2
+
+def main():
+    print(harj2.CalculateSum(5, 10))
+    print(harj2.CalculateDivision(100, 3))
+    
+    if __name__ == "__main__":
+        main()
---
+++ harj2.py
@@ -0,0 +1,12 @@
+def CalculateSUm(a,b):
+    return a +b
+def CalculateSubstract(a,b):
+    return a - b
+def CalculateMultiply(a,b):
+    return a * b
+def CalculateDivision(a,b):
+    if b == 0:
+        return 0:
+    return a/b
+   
+
